be41eaffa25190eac2e747dcda1865398ee47a3d
omergk28 <omergk28@gmail.com>
2025-03-08 18:42:16 -0500

    simplify test config
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>

--- src/test/java/com/omersoftware/cdc/postgresql/OrderApplicationTests.java
+++ src/test/java/com/omersoftware/cdc/postgresql/OrderApplicationTests.java
@@ -7,31 +7,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
 
 @SpringBootTest
 class OrderApplicationTests {
 
   static PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:16-alpine")
+      new PostgreSQLContainer<>("postgres:15")
           .withDatabaseName("orderdb")
           .withUsername("postgres")
-          .withPassword("postgres")
-          .withExposedPorts(5432);
+          .withPassword("postgres");
+
+  static KafkaContainer kafka = new KafkaContainer("apache/kafka-native:3.8.0");
 
   @DynamicPropertySource
-  static void registerPgProperties(DynamicPropertyRegistry registry) {
+  static void registerProperties(DynamicPropertyRegistry registry) {
     registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add("spring.kafka.consumer.bootstrap-servers", kafka::getBootstrapServers);
   }
 
   @BeforeAll
   static void beforeAll() {
     postgres.start();
+    kafka.start();
   }
 
   @AfterAll
   static void afterAll() {
+    kafka.stop();
     postgres.stop();
   }
 
