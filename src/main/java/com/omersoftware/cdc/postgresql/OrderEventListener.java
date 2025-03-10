13a285362ddbc865bc9e2a1134a12483c392e35e
omergk28 <omergk28@gmail.com>
2025-03-09 21:19:39 -0400

    add monitoring info, pretty print CDC events
    Signed-off-by: Omer Kocaoglu <omergk28@gmail.com>

--- src/main/java/com/omersoftware/cdc/postgresql/OrderEventListener.java
+++ src/main/java/com/omersoftware/cdc/postgresql/OrderEventListener.java
@@ -1,5 +1,7 @@
 package com.omersoftware.cdc.postgresql;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,14 @@ public class OrderEventListener {
 
   @KafkaListener(topics = "cdc.orderdb.order", groupId = "order-group")
   public void listen(String message) {
-    log.info("Received CDC message: " + message);
+    log.info(
+        "Received CDC message: "
+            + new Gson()
+                .newBuilder()
+                .setPrettyPrinting()
+                .create()
+                .fromJson(message, JsonObject.class)
+                .toString());
     // Process the message
   }
 }
